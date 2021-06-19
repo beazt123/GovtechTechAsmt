c798e7ddc81d2af5e556ea00f3edd071dbdee3cf
beazt123 <lohzhixuan@live.com>
2021-06-19 19:08:56 +0800

    Tested on postman

--- projectLib/usersEndpt.py
+++ projectLib/usersEndpt.py
@@ -1,10 +1,34 @@
+from projectLib.utils import serverResponse
 from flask import request
+from .constants import USERS_REQ_LIMIT, SALARY
 from projectLib.request_validation import getUserEndpt_ReqValidator
+from marshmallow import ValidationError
 
 
 def addUsersEndpt(app, mongo):
     @app.route("/users", methods=['GET'])
     def getUsers():
         if request.method == 'GET':
-            args = request.args
-            fileName = args.to_dict()
+            args = request.args.to_dict()
+            try:
+                print(args)
+                cleaned_args = getUserEndpt_ReqValidator().load(args)
+            except ValidationError as e:
+                return serverResponse(str(e), 401, "Invalid request")
+
+            foundUsers = mongo.db.users.find(
+                {
+                    SALARY: {
+                        "$lt": cleaned_args["maxSalary"],
+                        "$gt": cleaned_args["minSalary"]
+                        }
+                },
+                limit = USERS_REQ_LIMIT,
+                skip = cleaned_args['offset']
+                )
+            data = {"results": [user for user in foundUsers]}
+            return serverResponse(
+                data,
+                200,
+                f"{len(data['results'])} users found"
+                )
