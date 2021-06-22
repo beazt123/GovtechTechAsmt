bf72523585dad856f42fd12335c3defc4315f17f
beazt123 <50363877+beazt123@users.noreply.github.com>
2021-06-22 17:07:39 +0800

    Removed mongo URI for security reasons

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from projectLib.usersEndpt import addUsersEndpt
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGO_URI"] = "mongodb+srv://beazt123:wushuer1997@govtechemp.4a9zv.mongodb.net/govtechEmp?retryWrites=true&w=majority"
+    app.config["MONGO_URI"] = "YOUR MONGO URI HERE"
     app.config["SECRET_KEY"] = "U5WG0MYyEDf6TnMlN7bVAQTxf5iMHc2iQUU9RJIyZz0"
     mongo.init_app(app)
     addUploadCsvEndpt(app, mongo)
